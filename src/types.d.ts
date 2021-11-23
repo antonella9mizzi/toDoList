535891ef361b2de27d1bbdc37202bcd897fd4109
Antonella <contactamizzi@gmail.com>
2021-11-23 19:32:21 +0100

    type ts file created

---
+++ src/types.d.ts
@@ -0,0 +1,10 @@
+type AddItem = (newItem: string) => void;
+
+type Task = {
+    text: string,
+    complete: boolean,
+};
+
+type ToggleComplete = (selectedTodo:Task) => void;
+
+type HandleRemove = (taskToDelete:string) => void ;
